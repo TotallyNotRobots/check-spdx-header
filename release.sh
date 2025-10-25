fc1de07955a2e67ee7c4d1ba819711520eb6820c
linuxdaemon <linuxdaemon.irc@gmail.com>
2025-10-25 08:08:19 +0000

    build: update release script

--- release.sh
+++ release.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 changelog_file="$(mktemp)"
 cz bump --changelog-to-stdout --git-output-to-stderr > "$changelog_file"
 
-git push origin main --tags
-
-gh release create "$(cz version -p)" -d -F "$changelog_file"
+git push origin main
+sleep 1
+git push --tags
+sleep 1
+gh release create "$(cz version -p)" -t "Release $(cz version -p)" -F "$changelog_file"
 rm "$changelog_file"
